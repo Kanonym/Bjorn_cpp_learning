b29e9b73e2f3aa75beec0cacc3f9b8468e65bbaa
Kanonym <keyanoush@gmx.de>
2016-12-24 11:06:13 -0800

    Initial Commit

---
+++ Bjorn_cpp_learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bjorn_cpp_learning
+//
+//  Created by Keyanoush Razavi on 24.12.16.
+//  Copyright © 2016 Kanonymous. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
